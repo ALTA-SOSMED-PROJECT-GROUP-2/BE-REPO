10a10ec60bb315f961d2da4733c4877588b0deb2
Hanapibagas <hanafibagas4@gmail.com>
2023-12-06 20:03:32 +0800

    update fitur

--- routes/route.go
+++ routes/route.go
@@ -1,22 +1,29 @@
 package routes
 
 import (
+	"BE-REPO/features/posting"
 	"BE-REPO/features/user"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
-func InitRoute(e *echo.Echo, uc user.Handler) {
+func InitRoute(e *echo.Echo, uc user.Handler, pu posting.Handler) {
 	e.Pre(middleware.RemoveTrailingSlash())
 
 	e.Use(middleware.CORS())
 	e.Use(middleware.Logger())
 
 	RouteUser(e, uc)
+	RoutePosting(e, pu)
 }
 
 func RouteUser(e *echo.Echo, uc user.Handler) {
 	e.POST("/register", uc.Register())
 	e.POST("/login", uc.Login())
+}
+
+func RoutePosting(e *echo.Echo, pu posting.Handler) {
+	e.POST("/posting", pu.Add(), echojwt.JWT([]byte("$!1gnK3yyy!!!")))
 }
